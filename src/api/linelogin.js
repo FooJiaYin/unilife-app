270552271f22cd11884baa2b3ca753086f3486f6
FooJiaYin <candy23198@hotmail.com>
2022-05-10 10:34:32 +0800

    feat: Line login button

---
+++ src/api/linelogin.js
@@ -0,0 +1,10 @@
+
+const line = {
+    clientId: '1657093699',
+    redirectUrl: 'https://us-central1-gleaming-bot-319115.cloudfunctions.net/auth/line',
+    scope: 'profile openid email'
+}
+
+export const LineLoginUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${line.clientId}&redirect_uri=${encodeURIComponent(line.redirectUrl)}&scope=${encodeURI(line.scope)}&state=12345&nonce=6789&bot_prompt=aggressive`;
+
+console.log(LineLoginUrl)
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Dimension, ImageBackground, Image, Text, TextInput, Alert, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Dimension, ImageBackground, Image, Text, Linking, Alert, View } from 'react-native'
 import { setHeaderOptions } from '../components/navigation'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import Asset from '../components/assets'
@@ -9,22 +9,21 @@ import { stylesheet } from '../styles'
 import { firebase } from '../firebase/config'
 import { CurvedBg } from '../components/decorative'
 import * as WebBrowser from 'expo-web-browser';
+import { LineLoginUrl } from '../api/linelogin';
 
-export default function LoginScreen({navigation}) {
+export default function LoginScreen({navigation, ...props}) {
     const usersRef = firebase.firestore().collection('users')
-    
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const authToken = props.route.params && props.route.params.token
+    console.log(props.route.params)
 
     setHeaderOptions(navigation)
 
-    const onFooterLinkPress = () => {
-        navigation.navigate('Registration')
-        // WebBrowser.openBrowserAsync('https://supr.link/kmbjI');
-        // Linking.openURL('https://supr.link/kmbjI')
+    const onLoginPress = () => {
+        // WebBrowser.openBrowserAsync(LineLoginUrl)
+        Linking.openURL(LineLoginUrl)
     }
 
-    const onLoginPress = () => {
+    const login = (token) => {
         firebase
             .auth()
             .signInWithEmailAndPassword(email, password)
@@ -55,6 +54,7 @@ export default function LoginScreen({navigation}) {
                 else Alert.alert(error)
             })
     }
+
     React.useLayoutEffect(() => {
         navigation.setOptions({
             headerShown: false
@@ -64,37 +64,11 @@ export default function LoginScreen({navigation}) {
         <View style={{ flex: 1, width: '100%', justifyContent: 'center' }}>
             <ImageBackground source={Asset('bg-login.jpg')} resizeMode="cover" style={styles.bg}>
             </ImageBackground>
-            <KeyboardAwareScrollView
-                style={{ flex: 1, width: '100%'}}>
             <Image
                 style={styles.logo}
                 source={Asset('logo_with_text.png')}
             />
-            <View style={styles.card}>
-                <TextInput
-                    style={styles.input}
-                    placeholder='Email'
-                    placeholderTextColor="#aaaaaa"
-                    // defaultValue={'student@test.com'}
-                    onChangeText={(text) => setEmail(text)}
-                    value={email}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
-                />
-                <PasswordInput
-                    placeholder='密碼'
-                    onChangeText={(text) => setPassword(text)}
-                    value={password}
-                />
-                <Text onPress={() => navigation.navigate('ResetPassword')} style={[stylesheet.textGrey, {alignSelf: 'flex-end', marginTop: 4, marginBottom: 20}]}>
-                    忘記密碼？
-                </Text>
-                <Button onPress={onLoginPress} style={stylesheet.bgGreen} title="登入" />
-            </View>
-            <View style={stylesheet.footerView}>
-                <Text style={stylesheet.footerText}>沒有帳號？<Text onPress={onFooterLinkPress} style={stylesheet.footerLink}>現在註冊</Text></Text>
-            </View>
-            </KeyboardAwareScrollView>
+            <Button onPress={() => onLoginPress()} style={[stylesheet.bgGreen, {margin: 60, marginTop: 200}]} title="LINE 登入" />
         </View>
     )
 }
