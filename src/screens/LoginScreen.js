bd739be3302f410ad31706eadcbcd1dbf02a6950
FooJiaYin <candy23198@hotmail.com>
2023-06-02 13:37:55 +0800

    refactor(auth): ResetPasswordScreen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { Platform, Dimension, ImageBackground, Image, Text, Linking, Alert, View } from 'react-native'
 import { setHeaderOptions } from '../components/navigation'
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import Asset from '../components/assets'
 import { Button, PasswordInput } from '../components/forms'
 import { styles } from './LoginScreen/styles'
 import { stylesheet } from '../styles'
 import { firebase } from '../firebase/config'
-import { CurvedBg } from '../components/decorative'
 import * as WebBrowser from 'expo-web-browser';
 import * as AppleAuthentication from 'expo-apple-authentication';
 import { LineLoginUrl } from '../api/linelogin';
@@ -146,70 +144,6 @@ export default function LoginScreen({navigation, ...props}) {
                 <Text></Text>
                 <Text style={stylesheet.footerText}>遇到問題嗎？<Text onPress={()=>WebBrowser.openBrowserAsync("https://supr.link/kmc3i")} style={stylesheet.footerLink}>聯絡客服</Text></Text>
             </View>
-        </View>
-    )
-}
-
-export function ResetPasswordScreen({navigation}) {
-    const usersRef = firebase.firestore().collection('users')
-    
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [resetPassword, setResetPassword] = useState(false)
-
-    setHeaderOptions(navigation)
-
-    const sendPasswordResetEmail = () => {
-        firebase.auth().sendPasswordResetEmail(email, null)
-        .then(function() {
-            // Password reset email sent.
-            Alert.alert("重置密碼郵件已發送", "請前往信箱點擊連結，重設您的密碼")
-        })
-        .catch(function(error) {
-            if(error.code == 'auth/user-not-found' || error.code == 'auth/invalid-email') Alert.alert("該用戶不存在", "請檢查您輸入的信箱是否與註冊信箱相同。如需更改信箱，請洽客服協助。")
-            console.log(error)
-        // Error occurred. Inspect error.code.
-        });
-    }
-
-    const onFooterLinkPress = () => {
-        navigation.navigate('Registration')
-        // WebBrowser.openBrowserAsync('https://supr.link/kmbjI');
-        // Linking.openURL('https://supr.link/kmbjI')
-    }
-
-    React.useLayoutEffect(() => {
-        navigation.setOptions({
-            headerShown: false
-        })
-      }, [navigation])
-    return (
-        <View style={{ flex: 1, width: '100%', justifyContent: 'center' }}>
-            <ImageBackground source={Asset('bg-login.jpg')} resizeMode="cover" style={styles.bg}>
-            </ImageBackground>
-            <KeyboardAwareScrollView
-                style={{ flex: 1, width: '100%'}}>
-            <Image
-                style={styles.logo}
-                source={Asset('logo_with_text.png')}
-            />
-            <View style={styles.card}>
-                <TextInput
-                    style={[styles.input]}
-                    placeholder='Email'
-                    placeholderTextColor="#aaaaaa"
-                    // defaultValue={'student@test.com'}
-                    onChangeText={(text) => setEmail(text)}
-                    value={email}
-                    underlineColorAndroid="transparent"
-                    autoCapitalize="none"
-                />
-                <Button onPress={sendPasswordResetEmail} style={stylesheet.bgGreen} title="重設密碼" />
-            </View>
-            <View style={stylesheet.footerView}>
-                <Text style={stylesheet.footerText}>需要協助嗎？<Text onPress={()=>WebBrowser.openBrowserAsync("https://supr.link/WMlqU")} style={stylesheet.footerLink}>聯絡客服</Text></Text>
-            </View>
-            </KeyboardAwareScrollView>
         </View>
     )
 }
