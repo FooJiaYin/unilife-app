ebc97fb8b281d621c78b04f742b0b3e3a3f9e4f6
FooJiaYin <candy23198@hotmail.com>
2023-11-09 01:08:02 +0800

    fix: Crash after create article

--- src/screens/CommunityScreen.js
+++ src/screens/CommunityScreen.js
@@ -95,7 +95,15 @@ export default function CommunityScreen(props) {
                     //     newArticles[article.community].push(article)
                     // }
                     if (! user.hiddenArticles?.includes(snapshot.id) && ! user.hiddenSources?.includes(article.publishedBy)) {
-                        newArticles['all'].push(article)
+                        newArticles['all'].push({
+                            ...article, 
+                            // NOTE: This is a workaround to avoid crash after creating articles
+                            // Cause:
+                            // The id field is updated after creating article
+                            // When this onSnapshot triggered (article created), 
+                            // the id field is not yet updated
+                            id: snapshot.id,
+                        })
                     }
                     // firebase.firestore().collection('behavior').where('user','==', props.user.id).where('article', '==', snapshot.id).get().then(querySnapshot => {
                     //     if(querySnapshot.docs.length == 0) {
