30e82e7e434ca39af8a57ed83a5562ad60182bbb
FooJiaYin <candy23198@hotmail.com>
2023-11-09 09:38:35 +0800

    feat: Send line message on report

--- src/components/report.js
+++ src/components/report.js
@@ -1,13 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Alert, View, TouchableOpacity, Text, TextInput, Image, Share, Modal, FlatList } from 'react-native';
+import * as WebBrowser from 'expo-web-browser';
 import { stylesheet, Color } from '../styles';
 import { Icon } from './assets';
 import { firebase } from '../firebase/config';
 import { remove } from '../utils/array'
+import time from "../utils/time";
 
 export function ReportModal({ visible, onClose, article, user }) {
     function reportArticle(articleId) {
-        Alert.alert('', "已收到您的舉報，我們會盡快處理，謝謝您的回報。");
+        const reportMessage = `我要檢舉社群貼文
+
+貼文ID：
+${articleId}（請勿修改）
+貼文標題：
+${article.title}
+由 ${article.source} 於 ${time(article.publishedAt).format('YYYY/M/D hh:mm')} 發佈
+
+檢舉原因：
+<請自行填入>
+
+檢舉者：${user.info.nickname}（Email: ${user.info.email})`
+
+        Alert.alert('確定要檢舉此貼文嗎？', 
+            `按下確認後，將會開啟LINE客服人員聊天室。請務必填寫檢舉原因，否則將無法處理您的檢舉。\r\n如您無意檢舉此貼文，請按下取消。`,
+            [
+                {
+                    text: "取消",
+                    style: "cancel"
+                },
+                { text: "確認", onPress: () => WebBrowser.openBrowserAsync(`https://line.me/R/oaMessage/@221zrpwu/?${encodeURIComponent(reportMessage)}`) }
+            ]
+        );
         onClose();
     }
 
@@ -39,14 +63,14 @@ export function ReportModal({ visible, onClose, article, user }) {
             <View style={{ paddingHorizontal: 10 }}>
                 <TouchableOpacity style={{ ...stylesheet.row, alignItems: 'center' }} onPress={() => reportArticle(article.id)}>
                     <Icon name='report' size={20} style={{ marginRight: 10 }} />
-                    <Text style={[stylesheet.text, stylesheet.textBold, { fontSize: 17 }]}>舉報此貼文</Text>
+                    <Text style={[stylesheet.text, stylesheet.textBold, { fontSize: 17 }]}>向客服人員檢舉此貼文</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={{ ...stylesheet.row, alignItems: 'center' }} onPress={() => hideArticle(article.id)}>
-                    <Icon name='report' size={20} style={{ marginRight: 10 }} />
+                    <Icon name='hide' size={20} style={{ marginRight: 10 }} />
                     <Text style={[stylesheet.text, stylesheet.textBold, { fontSize: 17 }]}>隱藏此貼文</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={{ ...stylesheet.row, alignItems: 'center' }} onPress={() => hideSource(article.publishedBy)}>
-                    <Icon name='report' size={20} style={{ marginRight: 10 }} />
+                    <Icon name='hide-user' size={20} style={{ marginRight: 10 }} />
                     <Text style={[stylesheet.text, stylesheet.textBold, { fontSize: 17 }]}>隱藏所有來自 {article.source} 的貼文</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={{ ...stylesheet.row, justifyContent: 'flex-end' }} onPress={() => onClose()}>
